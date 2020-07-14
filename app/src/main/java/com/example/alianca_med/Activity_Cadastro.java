33ce5e447fff4219925c50d02166f47f63401818
VieiraMidas <guilherme.brasil3@etec.sp.gov.br>
2020-07-13 22:18:18 -0300

    Telas criadas, sem interações

---
+++ app/src/main/java/com/example/alianca_med/Activity_Cadastro.java
@@ -0,0 +1,14 @@
+package com.example.alianca_med;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout__cadastro);
+    }
+}
